2c11c484cce2a34544054fe101c3823ee5880896
aliHassanrabia <alihrabie@gmail.com>
2024-10-20 13:43:49 +0300

    update user

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,85 +1,96 @@
-import { Link } from "react-router-dom";
-import { FaHome, FaPowerOff, FaTasks, FaPlus } from "react-icons/fa";
-import { RiLoginBoxLine, RiUserAddLine } from "react-icons/ri";
+import { Link } from 'react-router-dom'
+import { FaHome, FaPowerOff, FaTasks, FaPlus } from 'react-icons/fa'
+import { RiLoginBoxLine, RiUserAddLine } from 'react-icons/ri'
 
 const Nav = ({ user, handleLogOut }) => {
-  const isAdmin = user && user.role === "admin";
+  const isAdmin = user && user.role === 'admin'
 
   const adminOptions = (
     <nav className="nav">
       <ul className="nav-list">
         <li>
           <Link to="/" className="nav-link">
-            <FaHome /> Home
+            {' '}
+            <FaHome /> Home{' '}
           </Link>
         </li>
         <li>
           <Link onClick={handleLogOut} to="/" className="nav-link">
-            <FaPowerOff /> Sign Out
+            {' '}
+            <FaPowerOff /> Sign Out{' '}
           </Link>
         </li>
         <li>
           <Link to="/tasks" className="nav-link">
-            <FaTasks /> Task List
+            {' '}
+            <FaTasks /> Task List{' '}
           </Link>
         </li>
         <li>
           <Link to="/tasks/new" className="nav-link">
-            <FaPlus /> Add Task
+            {' '}
+            <FaPlus /> Add Task{' '}
           </Link>
         </li>
       </ul>
     </nav>
-  );
+  )
 
   const userOptions = (
     <nav className="nav">
       <ul className="nav-list">
         <li>
           <Link to="/" className="nav-link">
-            <FaHome /> Home
+            {' '}
+            <FaHome /> Home{' '}
           </Link>
         </li>
         <li>
           <Link onClick={handleLogOut} to="/" className="nav-link">
-            <FaPowerOff /> Sign Out
+            {' '}
+            <FaPowerOff /> Sign Out{' '}
           </Link>
         </li>
         <li>
           <Link to="/tasks" className="nav-link">
-            <FaTasks /> Task List
+            {' '}
+            <FaTasks /> Task List{' '}
           </Link>
         </li>
         <li>
           <Link to="/tasks/new" className="nav-link">
-            <FaPlus /> Add Task
+            {' '}
+            <FaPlus /> Add Task{' '}
           </Link>
         </li>
       </ul>
     </nav>
-  );
+  )
 
   const publicOptions = (
     <nav className="nav">
       <ul className="nav-list">
         <li>
           <Link to="/" className="nav-link">
-            <FaHome /> Home
+            {' '}
+            <FaHome /> Home{' '}
           </Link>
         </li>
         <li>
           <Link to="/register" className="nav-link">
-            <RiUserAddLine /> Register
+            {' '}
+            <RiUserAddLine /> Register{' '}
           </Link>
         </li>
         <li>
           <Link to="/login" className="nav-link">
-            <RiLoginBoxLine /> Sign In
+            {' '}
+            <RiLoginBoxLine /> Sign In{' '}
           </Link>
         </li>
       </ul>
     </nav>
-  );
+  )
 
   return (
     <header>
@@ -87,7 +98,7 @@ const Nav = ({ user, handleLogOut }) => {
         {isAdmin ? adminOptions : user ? userOptions : publicOptions}
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Nav;
+export default Nav
