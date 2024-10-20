827f232a9aae60e48d7c64805770e1a82bd00970
aliHassanrabia <alihrabie@gmail.com>
2024-10-20 09:39:03 +0300

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import "./App.css"
+import { useState, useEffect } from "react"
+import RegisterPage from "./pages/RegisterPage"
+import LoginPage from "./pages/LoginPage"
 
-function App() {
-  return <div></div>
+const App = () => {
+  const [user, setUser] = useState({ data: null, role: null })
+
+  const handleLogOut = () => {
+    setUser({ data: null, role: null })
+  }
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const userData = await CheckSession()
+      setUser({ data: userData, role: userData.role })
+    }
+    checkToken()
+  }, [])
+
+  console.log(user)
+
+  return (
+    <div className="app-container">
+      <div className="main-content">
+        <Nav user={user.data} handleLogOut={handleLogOut} />
+        <main>
+          <Routes>
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage setUser={setUser} />} />
+          </Routes>
+        </main>
+        <div className="footer">LearnTech University &copy;2024 </div>
+      </div>
+    </div>
+  )
 }
+
+
+
 
 export default App
