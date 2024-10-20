c45af35748e0c047828827f717b9d6edb9cce1a0
aliHassanrabia <alihrabie@gmail.com>
2024-10-20 10:12:27 +0300

    create and update TaskList page

---
+++ src/pages/TaskList.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import TaskCard from "../components/TaskCard"; // Assuming you have a TaskCard component for individual tasks
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "../css/TaskList.css"; // Import the CSS file for styles
+
+const BASE_URL = "http://localhost:3000";
+
+const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  const getTasks = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/tasks`);
+      setTasks(response.data);
+    } catch (error) {
+      console.error("Failed to fetch tasks:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="tasklist-title">Task List</h1>
+      <Link to="/tasks/new" className="new-task-button">Create New Task</Link>
+      <div className="tasks">
+        {tasks.map((task) => (
+          <div key={task._id}>
+            <TaskCard task={task} /> {/* Ensure this component displays task details */}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;
