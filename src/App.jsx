a66aadf959c715fbcbdaf2e764224a5e3c6c6d83
aliHassanrabia <alihrabie@gmail.com>
2024-10-20 10:19:23 +0300

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import "./App.css"
-import { useState, useEffect } from "react"
-import RegisterPage from "./pages/RegisterPage"
-import LoginPage from "./pages/LoginPage"
-
+import "./App.css";
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import TaskList from "./pages/TaskList";  
+import TaskDetail from "./pages/TaskDetail"; 
+import NewTask from "./components/NewTask"; 
+import Nav from "./components/Nav"; 
+import { CheckSession } from "./api"; 
 const App = () => {
-  const [user, setUser] = useState({ data: null, role: null })
+  const [user, setUser] = useState({ data: null, role: null });
 
   const handleLogOut = () => {
-    setUser({ data: null, role: null })
-  }
+    setUser({ data: null, role: null });
+  };
 
   useEffect(() => {
     const checkToken = async () => {
-      const userData = await CheckSession()
-      setUser({ data: userData, role: userData.role })
-    }
-    checkToken()
-  }, [])
-
-  console.log(user)
+      const userData = await CheckSession();
+      setUser({ data: userData, role: userData.role });
+    };
+    checkToken();
+  }, []);
 
   return (
     <div className="app-container">
@@ -28,15 +31,16 @@ const App = () => {
           <Routes>
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/login" element={<LoginPage setUser={setUser} />} />
+            <Route path="/tasks" element={<TaskList />} />  
+            <Route path="/tasks/new" element={<NewTask user={user.data} />} />  
+            <Route path="/tasks/edit/:id" element={<NewTask user={user.data} />} />  
+            <Route path="/tasks/:id" element={<TaskDetail user={user.data} />} /> 
           </Routes>
         </main>
-        <div className="footer">LearnTech University &copy;2024 </div>
+        <div className="footer">LearnTech University &copy;2024</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-
-
-export default App
+export default App;
