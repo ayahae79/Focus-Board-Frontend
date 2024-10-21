37c4dc004c7cd4be565b4cbb53e4a604a21f645c
Ayah Ahmed <ayahahmed729@gmail.com>
2024-10-21 20:49:16 +0300

    all functions are working no errors

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,94 +1,99 @@
-import { Link } from 'react-router-dom'
-import { FaHome, FaPowerOff, FaTasks, FaPlus } from 'react-icons/fa'
-import { RiLoginBoxLine, RiUserAddLine } from 'react-icons/ri'
+import { Link } from "react-router-dom"
+import { FaHome, FaPowerOff, FaTasks, FaPlus, FaBook } from "react-icons/fa"
+import { RiLoginBoxLine, RiUserAddLine } from "react-icons/ri"
+import { GoProjectRoadmap } from "react-icons/go"
+import { MdEventNote } from "react-icons/md"
+import { CgProfile } from "react-icons/cg"
 
 const Nav = ({ user, handleLogOut }) => {
-  const isAdmin = user && user.role === 'admin'
+  const isAdmin = user && user.role === "admin"
 
   const adminOptions = (
     <nav className="nav">
       <ul className="nav-list">
         <li>
           <Link to="/" className="nav-link">
-            {' '}
-            <FaHome /> Home{' '}
+            {" "}
+            <FaHome /> Home{" "}
           </Link>
         </li>
         <li>
           <Link onClick={handleLogOut} to="/" className="nav-link">
-            {' '}
-            <FaPowerOff /> Sign Out{' '}
+            {" "}
+            <FaPowerOff /> Sign Out{" "}
           </Link>
         </li>
         <li>
           <Link to="/tasks" className="nav-link">
-            {' '}
-            <FaTasks /> Task List{' '}
+            {" "}
+            <FaTasks /> Task List{" "}
           </Link>
         </li>
         <li>
           <Link to="/tasks/new" className="nav-link">
-            {' '}
-            <FaPlus /> Add Task{' '}
+            {" "}
+            <FaPlus /> Add Task{" "}
           </Link>
         </li>
       </ul>
     </nav>
   )
-
   const userOptions = (
     <nav className="nav">
       <ul className="nav-list">
         <li>
+          <Link to="/profile/data" className="nav-link">
+            <CgProfile /> profile
+          </Link>
+        </li>
+        <li>
           <Link to="/" className="nav-link">
-            {' '}
-            <FaHome /> Home{' '}
+            <FaHome /> Home
           </Link>
         </li>
         <li>
           <Link onClick={handleLogOut} to="/" className="nav-link">
-            {' '}
-            <FaPowerOff /> Sign Out{' '}
+            <FaPowerOff /> Sign Out
           </Link>
         </li>
         <li>
           <Link to="/tasks" className="nav-link">
-            {' '}
-            <FaTasks /> Task List{' '}
+            <FaTasks /> Task List
           </Link>
         </li>
         <li>
           <Link to="/tasks/new" className="nav-link">
-            {' '}
-            <FaPlus /> Add Task{' '}
+            <FaPlus /> Add Task
           </Link>
         </li>
         <li>
-
+          <Link to="/roadmap" className="nav-link">
+            <GoProjectRoadmap /> Roadmap List
+          </Link>
+        </li>
+        <li>
           <Link to="/roadmap/new" className="nav-link">
-            {" "}
-            <FaPlus /> Add roadmap{" "}
-          <Link to="/events/add" className="nav-link">
-            {' '}
-            <FaPlus /> Add Event{' '}
+            <FaPlus /> Add Roadmap
           </Link>
         </li>
         <li>
           <Link to="/events" className="nav-link">
-            {' '}
-            <FaTasks /> Event List{' '}
+            <MdEventNote /> Event List
+          </Link>
+        </li>
+        <li>
+          <Link to="/events/add" className="nav-link">
+            <FaPlus /> Add Event
           </Link>
         </li>
         <li>
           <Link to="/courses" className="nav-link">
-            {' '}
-            <FaTasks /> Courses List{' '}
+            <FaBook /> Courses List
           </Link>
         </li>
         <li>
           <Link to="/courses/createcourse" className="nav-link">
-            {' '}
-            <FaPlus /> Add Course{' '}
+            <FaPlus /> Add Course
           </Link>
         </li>
       </ul>
@@ -100,20 +105,20 @@ const Nav = ({ user, handleLogOut }) => {
       <ul className="nav-list">
         <li>
           <Link to="/" className="nav-link">
-            {' '}
-            <FaHome /> Home{' '}
+            {" "}
+            <FaHome /> Home{" "}
           </Link>
         </li>
         <li>
           <Link to="/register" className="nav-link">
-            {' '}
-            <RiUserAddLine /> Register{' '}
+            {" "}
+            <RiUserAddLine /> Register{" "}
           </Link>
         </li>
         <li>
           <Link to="/login" className="nav-link">
-            {' '}
-            <RiLoginBoxLine /> Sign In{' '}
+            {" "}
+            <RiLoginBoxLine /> Sign In{" "}
           </Link>
         </li>
       </ul>
